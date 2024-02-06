4f307cc372b0ec7327afbb79306b09cf9281f3d3
Meysam <smprooichael@gmail.com>
2024-02-07 01:43:21 +0330

    7 acknowledge

--- receiver.py
+++ receiver.py
@@ -1,12 +1,19 @@
 import pika
+import time
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+crednetials = pika.PlainCredentials('Meysam', 'maisam')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=crednetials))
 ch = connection.channel()
 ch.queue_declare(queue='one')
 
 def callback(ch, method, properties, body):
     print(f'Received {body}')
+    print(method)
+    time.sleep(5)
+    print("Done!")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
+ch.basic_qos(prefetch_count=1)
 ch.basic_consume(queue='one', on_message_callback=callback, auto_ack=True)
 print('Waiting for messages. To exit press ctrl+c')
 ch.start_consuming()
--- sender.py
+++ sender.py
@@ -7,14 +7,6 @@ ch = connection.channel()
 
 ch.queue_declare(queue='one')
 ch.basic_publish(exchange='', routing_key='one', body='Hello world...', properties=pika.BasicProperties(
-    content_type="text/plain",
-    content_encoding="gzip",
-    timestamp=10000000,
-    expiration=str(time.time()),
-    user_id="10",
-    app_id="11",
-    type="exch.queue",
-    delivery_mode=2,
     headers={"name":"meysam", "age":"23"},
 ))
 print('Message was sent.')
